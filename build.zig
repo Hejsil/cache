950fc200366b8af44a70551a3e7e52a3a602eda9
Jimmi Holst Christensen <jhc@dismail.de>
2022-07-30 14:48:53 +0200

    Make it actually work

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    b.setPreferredReleaseMode(.ReleaseFast);
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -10,11 +11,4 @@ pub fn build(b: *std.build.Builder) void {
     exe.addPackagePath("clap", "lib/zig-clap/clap.zig");
     exe.addPackagePath("folders", "lib/known-folders/known-folders.zig");
     exe.install();
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -53,54 +53,45 @@ pub fn main() anyerror!void {
     });
     defer gba.free(cache_path);
 
-    var stdout_buffered = io.bufferedWriter(io.getStdErr().writer());
-    const stdout = stdout_buffered.writer();
-    var stderr_buffered = io.bufferedWriter(io.getStdErr().writer());
-    const stderr = stderr_buffered.writer();
+    const stdin = io.getStdIn();
+    const stdout = io.getStdErr().writer();
+    const stderr = io.getStdErr().writer();
 
     var diag = clap.Diagnostic{};
     const args = clap.parse(clap.Help, &params, parsers, .{
         .diagnostic = &diag,
     }) catch |err| {
         diag.report(stderr, err) catch {};
-        stderr_buffered.flush() catch {};
         return err;
     };
 
-    const digest = try digestFromArgs(gba, args);
+    const stdin_content = if (!stdin.isTty())
+        try stdin.readToEndAlloc(gba, math.maxInt(usize))
+    else
+        "";
+    defer gba.free(stdin_content);
 
-    const cache_miss = if (updateCache(
-        gba,
-        stdout,
-        stderr,
-        cache_path,
-        &digest,
-        args.args.output,
-    )) |_| false else |err| switch (err) {
-        error.FileNotFound => true,
+    const digest = try digestFromArgs(gba, stdin_content, args);
+    if (updateOutput(gba, stdout, stderr, cache_path, &digest, args.args.output)) |_| {
+        // Cache hit, just return
+        return;
+    } else |err| switch (err) {
+        error.FileNotFound => {
+            // Cache miss, execute command below
+        },
         else => |err2| return err2,
-    };
-
-    if (cache_miss) {
-        const output = try std.ChildProcess.exec(.{
-            .allocator = gba,
-            .argv = args.positionals,
-            .max_output_bytes = math.maxInt(usize),
-        });
-        _ = output;
-
-        try updateCache(
-            gba,
-            stdout,
-            stderr,
-            cache_path,
-            &digest,
-            args.args.output,
-        );
     }
 
-    try stdout_buffered.flush();
-    try stderr_buffered.flush();
+    const output = try std.ChildProcess.exec(.{
+        .allocator = gba,
+        .argv = args.positionals,
+        .max_output_bytes = math.maxInt(usize),
+    });
+    defer gba.free(output.stdout);
+    defer gba.free(output.stderr);
+
+    try updateCache(gba, output.stdout, output.stderr, cache_path, &digest, args.args.output);
+    try updateOutput(gba, stdout, stderr, cache_path, &digest, args.args.output);
 }
 
 const BinDigest = [bin_digest_len]u8;
@@ -108,9 +99,14 @@ const bin_digest_len = 16;
 const Hasher = crypto.auth.siphash.SipHash128(1, 3);
 const hex_digest_len = bin_digest_len * 2;
 
-fn digestFromArgs(allocator: mem.Allocator, args: anytype) ![hex_digest_len]u8 {
+fn digestFromArgs(
+    allocator: mem.Allocator,
+    stdin: []const u8,
+    args: anytype,
+) ![hex_digest_len]u8 {
     const cwd = fs.cwd();
     var hasher = Hasher.init(&[_]u8{0} ** Hasher.key_length);
+    hasher.update(stdin);
 
     for (args.positionals) |command_arg|
         hasher.update(command_arg);
@@ -122,11 +118,16 @@ fn digestFromArgs(allocator: mem.Allocator, args: anytype) ![hex_digest_len]u8 {
         hasher.update(env);
         hasher.update(content);
     }
-    for (args.args.file) |file| {
-        const content = try cwd.readFileAlloc(allocator, file, math.maxInt(usize));
-        defer allocator.free(content);
-        hasher.update(file);
-        hasher.update(content);
+    for (args.args.file) |filepath| {
+        const realpath = try cwd.realpathAlloc(allocator, filepath);
+        defer allocator.free(realpath);
+
+        const file = try cwd.openFile(realpath, .{});
+        const metadata = try file.metadata();
+        defer file.close();
+        hasher.update(realpath);
+        hasher.update(&mem.toBytes(metadata.size()));
+        hasher.update(&mem.toBytes(metadata.modified()));
     }
 
     var bin_digest: BinDigest = undefined;
@@ -141,7 +142,7 @@ fn digestFromArgs(allocator: mem.Allocator, args: anytype) ![hex_digest_len]u8 {
     return res;
 }
 
-fn updateCache(
+fn updateOutput(
     allocator: mem.Allocator,
     stdout: anytype,
     stderr: anytype,
@@ -171,10 +172,46 @@ fn updateCache(
             fmt.bufPrint(&buf, "{s}-{}", .{ digest, i }) catch unreachable,
         });
         defer allocator.free(path);
-        try cwd.symLink(output, path, .{});
+
+        cwd.deleteFile(output) catch |err| switch (err) {
+            error.FileNotFound => {},
+            else => |e| return e,
+        };
+        try cwd.symLink(path, output, .{});
     }
 
     var fifo = std.fifo.LinearFifo(u8, .{ .Static = mem.page_size }).init();
     try fifo.pump(stdout_file.reader(), stdout);
     try fifo.pump(stderr_file.reader(), stderr);
+}
+
+fn updateCache(
+    allocator: mem.Allocator,
+    stdout: []const u8,
+    stderr: []const u8,
+    cache_path: []const u8,
+    digest: []const u8,
+    outputs: []const []const u8,
+) !void {
+    var buf: [1024]u8 = undefined;
+    const cwd = fs.cwd();
+    const stdout_path = try fs.path.join(allocator, &.{
+        cache_path,
+        fmt.bufPrint(&buf, "{s}-stdout", .{digest}) catch unreachable,
+    });
+    defer allocator.free(stdout_path);
+    const stderr_path = try fs.path.join(allocator, &.{
+        cache_path,
+        fmt.bufPrint(&buf, "{s}-stderr", .{digest}) catch unreachable,
+    });
+    defer allocator.free(stderr_path);
+
+    try cwd.writeFile(stdout_path, stdout);
+    try cwd.writeFile(stderr_path, stderr);
+
+    const cache_dir = try cwd.openDir(cache_path, .{});
+    for (outputs) |output, i| {
+        const cache_name = fmt.bufPrint(&buf, "{s}-{}", .{ digest, i }) catch unreachable;
+        try fs.rename(cwd, output, cache_dir, cache_name);
+    }
 }
