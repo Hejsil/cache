618b0c127a60be794762ec0f5e0b7ec38002bffb
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-07-07 01:28:21 +0200

    build: Add option to strip binary

--- build.zig
+++ build.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+    const strip = b.option(bool, "strip", "Omit debug symbols") orelse false;
 
     const exe = b.addExecutable(.{
         .name = "cache",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .strip = strip,
     });
     exe.root_module.addAnonymousImport("clap", .{
         .root_source_file = b.path("lib/zig-clap/clap.zig"),
