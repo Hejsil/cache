6d2f6ca4972757aec0d83798fdf5647cd7264614
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-08-22 21:13:31 +0200

    chore: Update to zig 0.15.1

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,3 @@
-const spaghet = @import("spaghet");
-const folders = @import("folders");
-const std = @import("std");
-
-const crypto = std.crypto;
-const fmt = std.fmt;
-const fs = std.fs;
-const heap = std.heap;
-const io = std.io;
-const math = std.math;
-const mem = std.mem;
-const process = std.process;
-
 const usage =
     \\Usage: cache [options]
     \\
@@ -58,7 +45,7 @@ const Args = struct {
 };
 
 pub fn main() !void {
-    var gpa_state = heap.GeneralPurposeAllocator(.{}){};
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = gpa_state.allocator();
     defer _ = gpa_state.deinit();
 
@@ -66,21 +53,42 @@ pub fn main() !void {
     const arena = arena_state.allocator();
     defer arena_state.deinit();
 
-    const stdin = std.fs.File.stdin();
-    const stdout = std.fs.File.stdout();
-    const stderr = std.fs.File.stderr();
+    var stdin_buf: [std.heap.page_size_min]u8 = undefined;
+    var stdout_buf: [std.heap.page_size_min]u8 = undefined;
+    var stderr_buf: [std.heap.page_size_min]u8 = undefined;
+    var stdin = std.fs.File.stdin().reader(&stdin_buf);
+    var stdout = std.fs.File.stdout().writer(&stdout_buf);
+    var stderr = std.fs.File.stderr().writer(&stderr_buf);
 
-    const global_cache_path = (try folders.getPath(arena, .cache)) orelse return error.MissingGlobalCache;
-    const cache_path = try fs.path.join(arena, &.{ global_cache_path, "cache" });
+    try mainFull(.{
+        .arena = arena,
+        .stdin = &stdin,
+        .stdout = &stdout,
+        .stderr = &stderr,
+    });
+    try stdout.end();
+    try stderr.end();
+}
+
+const Io = struct {
+    arena: std.mem.Allocator,
+    stdout: *std.fs.File.Writer,
+    stderr: *std.fs.File.Writer,
+    stdin: *std.fs.File.Reader,
+};
+
+fn mainFull(io: Io) !void {
+    const global_cache_path = (try folders.getPath(io.arena, .cache)) orelse return error.MissingGlobalCache;
+    const cache_path = try std.fs.path.join(io.arena, &.{ global_cache_path, "cache" });
 
     var cache_dir = try std.fs.cwd().makeOpenPath(cache_path, .{});
     defer cache_dir.close();
 
-    var arg_parser = try spaghet.Args.initArgs(arena);
+    var arg_parser = try spaghet.Args.initArgs(io.arena);
     var args = Args{};
     while (arg_parser.next()) {
         if (arg_parser.flag(&.{ "-h", "--help" }))
-            return stdout.writeAll(usage);
+            return io.stdout.interface.writeAll(usage);
         if (arg_parser.flag(&.{"--stdin"}))
             args.stdin = true;
         if (arg_parser.flag(&.{"--ignore-stdout"}))
@@ -88,25 +96,24 @@ pub fn main() !void {
         if (arg_parser.flag(&.{"--ignore-stderr"}))
             args.ignore_stderr = true;
         if (arg_parser.option(&.{ "-e", "--env" })) |v|
-            try args.envs.append(arena, v);
+            try args.envs.append(io.arena, v);
         if (arg_parser.option(&.{ "-f", "--file" })) |v|
-            try args.files.append(arena, v);
+            try args.files.append(io.arena, v);
         if (arg_parser.option(&.{ "-s", "--string" })) |v|
-            try args.strings.append(arena, v);
+            try args.strings.append(io.arena, v);
         if (arg_parser.option(&.{ "-o", "--output" })) |v|
-            try args.outputs.append(arena, v);
+            try args.outputs.append(io.arena, v);
         if (arg_parser.positional()) |v|
-            try args.command.append(arena, v);
+            try args.command.append(io.arena, v);
     }
 
     const stdin_content = if (args.stdin)
-        try stdin.readToEndAlloc(gpa, math.maxInt(usize))
+        try io.stdin.interface.allocRemaining(io.arena, .unlimited)
     else
         "";
-    defer gpa.free(stdin_content);
 
-    const digest = try digestFromArgs(gpa, stdin_content, args);
-    if (updateOutput(gpa, stdout, stderr, cache_dir, cache_path, &digest, args.outputs.items)) |_| {
+    const digest = try digestFromArgs(io.arena, stdin_content, args);
+    if (updateOutput(io, cache_dir, cache_path, &digest, args.outputs.items)) |_| {
         // Cache hit, just return
         return;
     } else |err| switch (err) {
@@ -117,38 +124,32 @@ pub fn main() !void {
     }
 
     const output = try std.process.Child.run(.{
-        .allocator = gpa,
+        .allocator = io.arena,
         .argv = args.command.items,
-        .max_output_bytes = math.maxInt(usize),
+        .max_output_bytes = std.math.maxInt(usize),
     });
-    defer gpa.free(output.stdout);
-    defer gpa.free(output.stderr);
 
     const output_stdout = if (!args.ignore_stdout) output.stdout else "";
     const output_stderr = if (!args.ignore_stderr) output.stderr else "";
 
-    try updateCache(output_stdout, output_stderr, cache_dir, &digest, args.outputs.items);
-    try updateOutput(gpa, stdout, stderr, cache_dir, cache_path, &digest, args.outputs.items);
+    try updateCache(io, output_stdout, output_stderr, cache_dir, &digest, args.outputs.items);
+    try updateOutput(io, cache_dir, cache_path, &digest, args.outputs.items);
 
     // Print out stdout and stderr when ignore but didn't have a cache hit. This allows for better
     // debugging if the command fails.
     if (!args.ignore_stdout)
-        try stdout.writeAll(output.stdout);
+        try io.stdout.interface.writeAll(output.stdout);
     if (!args.ignore_stderr)
-        try stderr.writeAll(output.stderr);
+        try io.stderr.interface.writeAll(output.stderr);
 }
 
 const BinDigest = [bin_digest_len]u8;
 const bin_digest_len = 16;
-const Hasher = crypto.auth.siphash.SipHash128(1, 3);
+const Hasher = std.crypto.auth.siphash.SipHash128(1, 3);
 const hex_digest_len = bin_digest_len * 2;
 
-fn digestFromArgs(
-    allocator: mem.Allocator,
-    stdin: []const u8,
-    args: Args,
-) ![hex_digest_len]u8 {
-    const cwd = fs.cwd();
+fn digestFromArgs(arena: std.mem.Allocator, stdin: []const u8, args: Args) ![hex_digest_len]u8 {
+    const cwd = std.fs.cwd();
     var hasher = Hasher.init(&[_]u8{0} ** Hasher.key_length);
     hasher.update(stdin);
 
@@ -157,24 +158,21 @@ fn digestFromArgs(
     for (args.strings.items) |string|
         hasher.update(string);
     for (args.envs.items) |env| {
-        const content = process.getEnvVarOwned(allocator, env) catch |err| switch (err) {
+        const content = std.process.getEnvVarOwned(arena, env) catch |err| switch (err) {
             error.EnvironmentVariableNotFound => "",
             else => |e| return e,
         };
-        defer allocator.free(content);
         hasher.update(env);
         hasher.update(content);
     }
     for (args.files.items) |filepath| {
-        const realpath = try cwd.realpathAlloc(allocator, filepath);
-        defer allocator.free(realpath);
-
+        const realpath = try cwd.realpathAlloc(arena, filepath);
         const file = try cwd.openFile(realpath, .{});
         const stat = try file.stat();
         defer file.close();
         hasher.update(realpath);
-        hasher.update(&mem.toBytes(stat.size));
-        hasher.update(&mem.toBytes(stat.mtime));
+        hasher.update(&std.mem.toBytes(stat.size));
+        hasher.update(&std.mem.toBytes(stat.mtime));
     }
 
     if (args.ignore_stdout)
@@ -189,23 +187,16 @@ fn digestFromArgs(
 }
 
 fn updateOutput(
-    allocator: mem.Allocator,
-    stdout: std.fs.File,
-    stderr: std.fs.File,
+    io: Io,
     cache_dir: std.fs.Dir,
     cache_path: []const u8,
     digest: []const u8,
     outputs: []const []const u8,
 ) !void {
-    var buf: [std.fs.max_name_bytes]u8 = undefined;
-
-    const cwd = fs.cwd();
+    const cwd = std.fs.cwd();
     for (outputs, 0..) |output, i| {
-        const path = try fs.path.join(allocator, &.{
-            cache_path,
-            try fmt.bufPrint(&buf, "{s}-{}", .{ digest, i }),
-        });
-        defer allocator.free(path);
+        const file_name = try std.fmt.allocPrint(io.arena, "{s}-{}", .{ digest, i });
+        const path = try std.fs.path.join(io.arena, &.{ cache_path, file_name });
 
         cwd.deleteFile(output) catch |err| switch (err) {
             error.FileNotFound => {},
@@ -214,44 +205,55 @@ fn updateOutput(
         try cwd.symLink(path, output, .{});
     }
 
-    const stdout_name = try fmt.bufPrint(&buf, "{s}-stdout", .{digest});
-    const stdout_file = try cache_dir.openFile(stdout_name, .{});
-    defer stdout_file.close();
-    try stdout.writeFileAll(stdout_file, .{});
+    const stdout_name = try std.fmt.allocPrint(io.arena, "{s}-stdout", .{digest});
+    const stderr_name = try std.fmt.allocPrint(io.arena, "{s}-stderr", .{digest});
 
-    const stderr_name = try fmt.bufPrint(&buf, "{s}-stderr", .{digest});
-    if (cache_dir.openFile(stderr_name, .{})) |stderr_file| {
-        defer stderr_file.close();
-        try stderr.writeFileAll(stderr_file, .{});
-    } else |err| switch (err) {
-        error.FileNotFound => {},
+    var stdout_file_buf: [std.heap.page_size_min]u8 = undefined;
+    var stderr_file_buf: [std.heap.page_size_min]u8 = undefined;
+
+    var stdout_file = (try cache_dir.openFile(stdout_name, .{})).reader(&stdout_file_buf);
+    defer stdout_file.file.close();
+
+    _ = try io.stdout.interface.sendFileAll(&stdout_file, .unlimited);
+
+    var stderr_file = (cache_dir.openFile(stderr_name, .{}) catch |err| switch (err) {
+        error.FileNotFound => return,
         else => |e| return e,
-    }
+    }).reader(&stderr_file_buf);
+    defer stderr_file.file.close();
+
+    _ = try io.stderr.interface.sendFileAll(&stderr_file, .unlimited);
 }
 
 fn updateCache(
+    io: Io,
     stdout: []const u8,
     stderr: []const u8,
     cache_dir: std.fs.Dir,
     digest: []const u8,
     outputs: []const []const u8,
 ) !void {
-    var buf: [std.fs.max_name_bytes]u8 = undefined;
-    const cwd = fs.cwd();
+    const cwd = std.fs.cwd();
 
-    const stdout_name = try fmt.bufPrint(&buf, "{s}-stdout", .{digest});
+    const stdout_name = try std.fmt.allocPrint(io.arena, "{s}-stdout", .{digest});
     try cache_dir.writeFile(.{ .sub_path = stdout_name, .data = stdout });
 
+    std.debug.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n", .{});
     if (stderr.len != 0) {
-        const stderr_name = try fmt.bufPrint(&buf, "{s}-stderr", .{digest});
+        std.debug.print("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n", .{});
+        const stderr_name = try std.fmt.allocPrint(io.arena, "{s}-stderr", .{digest});
         try cache_dir.writeFile(.{ .sub_path = stderr_name, .data = stderr });
     }
 
     for (outputs, 0..) |output, i| {
-        const cache_name = try fmt.bufPrint(&buf, "{s}-{}", .{ digest, i });
-        fs.rename(cwd, output, cache_dir, cache_name) catch |err| switch (err) {
+        const cache_name = try std.fmt.allocPrint(io.arena, "{s}-{}", .{ digest, i });
+        std.fs.rename(cwd, output, cache_dir, cache_name) catch |err| switch (err) {
             error.RenameAcrossMountPoints => try cwd.copyFile(output, cache_dir, cache_name, .{}),
             else => |e| return e,
         };
     }
 }
+
+const spaghet = @import("spaghet");
+const folders = @import("folders");
+const std = @import("std");
